c09988e94761c492c56e73bd52050269fa3a3e1a
maldron0309 <bagjinhyeong640@gmail.com>
2024-10-01 17:56:49 +0900

    feat: player horizontal move, map range system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    
+    private void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    }
+}
